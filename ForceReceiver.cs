78cb5ada7b522ec503df483245d2c9e9a9cc2fac
Kaka Dhika <mandhikarizq@gmail.com>
2024-04-03 17:16:02 +0700

    First Commit

---
+++ ForceReceiver.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ForceReceiver : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private float drag = 0.3f;
+
+    private Vector3 dampingVelocity;
+    private Vector3 impact;
+    private float verticalVelocity;
+
+    public Vector3 Movement => impact + Vector3.up * verticalVelocity;
+    // Update is called once per frame
+    void Update()
+    {
+        if (verticalVelocity < 0f && controller.isGrounded)
+        {
+            verticalVelocity = Physics.gravity.y * Time.deltaTime;
+        }else{
+            verticalVelocity += Physics.gravity.y * Time.deltaTime;
+        }
+
+        impact = Vector3.SmoothDamp(impact, Vector3.zero, ref dampingVelocity, drag);
+
+        if (agent != null)
+        {
+            if (impact == Vector3.zero)
+            {
+                agent.enabled = true;
+            }
+        }
+        
+    }
+
+    public void AddForce(Vector3 force)
+    {
+        impact += force;
+        if (agent != null)
+        {
+            agent.enabled = false;
+        }
+    }
+}
---
+++ StateMachine/Player/PlayerStateMachine.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : StateMachine
+{
+    [field: SerializeField] public InputReader InputReader {get; private set;}
+    [field: SerializeField] public CharacterController Controller {get; private set;}
+    [field: SerializeField] public Animator Animator {get; private set;}
+    [field: SerializeField] public Targeter Targeter {get; private set;}
+    [field: SerializeField] public ForceReceiver ForceReceiver {get; private set;}
+    [field: SerializeField] public WeaponDamage Weapon {get; private set;}
+    [field: SerializeField] public float FreeLookMovementSpeed {get; private set;}
+    [field: SerializeField] public float TargettingMovementSpeed {get; private set;}
+    [field: SerializeField] public float RotationDumping {get; private set;}
+    [field: SerializeField] public Attack[] Attacks{get; private set;}
+    public Transform MainCameraTransform {get; private set;}
+    void Start()
+    {
+        MainCameraTransform = Camera.main.transform;
+        SwitchState(new PlayerFreeLookState(this));
+    }
+
+}
