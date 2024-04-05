706c6f7393b61390960a7716f029c1b43dc2e347
Kaka Dhika <mandhikarizq@gmail.com>
2024-04-05 20:31:07 +0700

    Impact State Transitions

--- Combat/Health.cs
+++ Combat/Health.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class Health : MonoBehaviour
 
     private int health;
    
+    public event Action OnTakeDamage;
 
     private void Start()
     {
@@ -19,6 +21,8 @@ public class Health : MonoBehaviour
         if(health == 0) {return;}
 
         health = Mathf.Max(health - damage, 0);
+
+        OnTakeDamage?.Invoke();
 
         Debug.Log(health);
     }
